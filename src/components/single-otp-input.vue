2def8b8ee27c3605f37285407ec96b5abc72f40a
zhengyi <88762262+hauzhengyi@users.noreply.github.com>
2024-03-12 17:41:30 +0800

    iOS auto populate fix

--- src/components/single-otp-input.vue
+++ src/components/single-otp-input.vue
@@ -1,173 +1,168 @@
 <template>
-  <div style="display: flex; align-items: center">
-    <input
-      data-test="single-input"
-      :type="inputTypeValue"
-      :inputmode="inputmode"
-      :placeholder="placeholder"
-      :disabled="isDisabled"
-      ref="input"
-      min="0"
-      max="9"
-      maxlength="1"
-      pattern="[0-9]"
-      v-model="model"
-      :class="[inputClasses, conditionalClass, { 'is-complete': model }]"
-      @input="handleOnChange"
-      @keydown="handleOnKeyDown"
-      @paste="handleOnPaste"
-      @focus="handleOnFocus"
-      @blur="handleOnBlur"
-    />
-    <span v-if="!isLastChild && separator">
-      <span v-html="separator"></span>
-    </span>
-  </div>
+	<div style="display: flex; align-items: center">
+		<input
+			data-test="single-input"
+			:type="inputTypeValue"
+			:inputmode="inputmode"
+			:placeholder="placeholder"
+			:disabled="isDisabled"
+			ref="input"
+			min="0"
+			max="9"
+			maxlength="1"
+			pattern="[0-9]"
+			:value="model"
+			:class="[inputClasses, conditionalClass, { 'is-complete': model }]"
+			@input="handleOnChange"
+			@keydown="handleOnKeyDown"
+			@paste="handleOnPaste"
+			@focus="handleOnFocus"
+			@blur="handleOnBlur"
+		/>
+		<span v-if="!isLastChild && separator">
+			<span v-html="separator"></span>
+		</span>
+	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, PropType, ref, Ref, watch } from "vue";
 
 export default defineComponent({
-  name: "SingleOtpInput",
-  props: {
-    inputType: {
-      type: String as PropType<
-        "number" | "tel" | "letter-numeric" | "password"
-      >,
-      validator: (value: string) =>
-        ["number", "tel", "letter-numeric", "password"].includes(value),
-      default: "tel",
-    },
-    inputmode: {
-      type: String as PropType<
-        | "none"
-        | "text"
-        | "tel"
-        | "url"
-        | "email"
-        | "numeric"
-        | "decimal"
-        | "search"
-        /**
-         * Specify that a standard HTML element should behave like a defined custom built-in element
-         * @see https://html.spec.whatwg.org/multipage/custom-elements.html#attr-is
-         */
-      >,
-      default: "numeric",
-    },
-    value: {
-      type: [String, Number],
-    },
-    separator: {
-      type: String,
-    },
-    focus: {
-      type: Boolean,
-    },
-    inputClasses: {
-      type: [String, Array] as PropType<string[] | string>,
-    },
-    conditionalClass: {
-      type: String,
-    },
-    shouldAutoFocus: {
-      type: Boolean,
-    },
-    isLastChild: {
-      type: Boolean,
-    },
-    placeholder: {
-      type: String,
-    },
-    isDisabled: {
-      type: Boolean,
-    },
-  },
-  emits: ["on-change", "on-keydown", "on-paste", "on-focus", "on-blur"],
-  setup(props, { emit }) {
-    const model = ref(props.value || "");
-    const input = ref<HTMLInputElement | null>(null) as Ref<HTMLInputElement>;
+	name: "SingleOtpInput",
+	props: {
+		inputType: {
+			type: String as PropType<"number" | "tel" | "letter-numeric" | "password">,
+			validator: (value: string) => ["number", "tel", "letter-numeric", "password"].includes(value),
+			default: "tel",
+		},
+		inputmode: {
+			type: String as PropType<
+				"none" | "text" | "tel" | "url" | "email" | "numeric" | "decimal" | "search"
+				/**
+				 * Specify that a standard HTML element should behave like a defined custom built-in element
+				 * @see https://html.spec.whatwg.org/multipage/custom-elements.html#attr-is
+				 */
+			>,
+			default: "numeric",
+		},
+		value: {
+			type: [String, Number],
+		},
+		separator: {
+			type: String,
+		},
+		focus: {
+			type: Boolean,
+		},
+		inputClasses: {
+			type: [String, Array] as PropType<string[] | string>,
+		},
+		conditionalClass: {
+			type: String,
+		},
+		shouldAutoFocus: {
+			type: Boolean,
+		},
+		isLastChild: {
+			type: Boolean,
+		},
+		placeholder: {
+			type: String,
+		},
+		isDisabled: {
+			type: Boolean,
+		},
+	},
+	emits: ["on-change", "on-keydown", "on-paste", "on-focus", "on-blur"],
+	setup(props, { emit }) {
+		const model = ref(props.value || "");
+		const input = ref<HTMLInputElement | null>(null) as Ref<HTMLInputElement>;
 
-    const handleOnChange = () => {
-      model.value = model.value.toString();
-      if (model.value.length > 1) {
-        model.value = model.value.slice(0, 1);
-      }
-      return emit("on-change", model.value);
-    };
+		const handleOnChange = (e: Event) => {
+			const value = (e.target as HTMLInputElement).value;
+			if (value && value.trim().length > 1) {
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				// This is a workaround for dealing IOS does not fire onPaste event from sms auto-populate.
+				e.clipboardData = {
+					getData: () => value.trim(),
+				};
+				return emit("on-paste", e);
+			}
+			return emit("on-change", value);
+		};
 
-    const isCodeLetter = (charCode: number) => charCode >= 65 && charCode <= 90;
-    const isCodeNumeric = (charCode: number) =>
-      (charCode >= 48 && charCode <= 57) || (charCode >= 96 && charCode <= 105);
-    // numeric keys and numpad keys
+		const isCodeLetter = (charCode: number) => charCode >= 65 && charCode <= 90;
+		const isCodeNumeric = (charCode: number) =>
+			(charCode >= 48 && charCode <= 57) || (charCode >= 96 && charCode <= 105);
+		// numeric keys and numpad keys
 
-    const handleOnKeyDown = (event: KeyboardEvent) => {
-      if (props.isDisabled) {
-        event.preventDefault();
-      }
-      // Only allow characters 0-9, DEL, Backspace, Enter, Right and Left Arrows, and Pasting
-      const keyEvent = event || window.event;
-      const charCode = keyEvent.which ? keyEvent.which : keyEvent.keyCode;
-      if (
-        isCodeNumeric(charCode) ||
-        (props.inputType === "letter-numeric" && isCodeLetter(charCode)) ||
-        [8, 9, 13, 37, 39, 46, 86].includes(charCode)
-      ) {
-        emit("on-keydown", event);
-      } else {
-        keyEvent.preventDefault();
-      }
-    };
+		const handleOnKeyDown = (event: KeyboardEvent) => {
+			if (props.isDisabled) {
+				event.preventDefault();
+			}
+			// Only allow characters 0-9, DEL, Backspace, Enter, Right and Left Arrows, and Pasting
+			const keyEvent = event || window.event;
+			const charCode = keyEvent.which ? keyEvent.which : keyEvent.keyCode;
+			if (
+				isCodeNumeric(charCode) ||
+				(props.inputType === "letter-numeric" && isCodeLetter(charCode)) ||
+				[8, 9, 13, 37, 39, 46, 86].includes(charCode)
+			) {
+				emit("on-keydown", event);
+			} else {
+				keyEvent.preventDefault();
+			}
+		};
 
-    const handleOnPaste = (event: ClipboardEvent) => emit("on-paste", event);
+		const handleOnPaste = (event: ClipboardEvent) => emit("on-paste", event);
 
-    const handleOnFocus = () => {
-      input.value.select();
-      return emit("on-focus");
-    };
+		const handleOnFocus = () => {
+			input.value.select();
+			return emit("on-focus");
+		};
 
-    const handleOnBlur = () => emit("on-blur");
+		const handleOnBlur = () => emit("on-blur");
 
-    watch(
-      () => props.value,
-      (newValue, oldValue) => {
-        if (newValue !== oldValue) {
-          model.value = newValue as string | number;
-        }
-      }
-    );
-    watch(
-      () => props.focus,
-      (newFocusValue, oldFocusValue) => {
-        // Check if focusedInput changed
-        // Prevent calling function if input already in focus
-        if (oldFocusValue !== newFocusValue && input.value && props.focus) {
-          input.value.focus();
-          input.value.select();
-        }
-      }
-    );
+		watch(
+			() => props.value,
+			(newValue, oldValue) => {
+				if (newValue !== oldValue) {
+					model.value = newValue as string | number;
+				}
+			}
+		);
+		watch(
+			() => props.focus,
+			(newFocusValue, oldFocusValue) => {
+				// Check if focusedInput changed
+				// Prevent calling function if input already in focus
+				if (oldFocusValue !== newFocusValue && input.value && props.focus) {
+					input.value.focus();
+					input.value.select();
+				}
+			}
+		);
 
-    onMounted(() => {
-      if (input.value && props.focus && props.shouldAutoFocus) {
-        input.value.focus();
-        input.value.select();
-      }
-    });
+		onMounted(() => {
+			if (input.value && props.focus && props.shouldAutoFocus) {
+				input.value.focus();
+				input.value.select();
+			}
+		});
 
-    return {
-      handleOnChange,
-      handleOnKeyDown,
-      handleOnPaste,
-      handleOnFocus,
-      handleOnBlur,
-      input,
-      model,
-      inputTypeValue:
-        ["letter-numeric", "number"].includes(props.inputType) ? "text" : props.inputType,
-    };
-  },
+		return {
+			handleOnChange,
+			handleOnKeyDown,
+			handleOnPaste,
+			handleOnFocus,
+			handleOnBlur,
+			input,
+			model,
+			inputTypeValue: ["letter-numeric", "number"].includes(props.inputType) ? "text" : props.inputType,
+		};
+	},
 });
 </script>
 
